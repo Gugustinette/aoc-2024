9a9278f631bf7251af99ff127931090f1f59270d
Gugustinette <mercier.augustin@outlook.fr>
2024-12-04 18:09:55 +0100

    feat: finish day 4

--- src/day-4/index.ts
+++ src/day-4/index.ts
@@ -13,35 +13,74 @@ function loadData() : string {
 function part1(): number {
   // Load data
   const data = loadData()
-  // Get length of a line in the input data
-  const lineLenght = data.split("\n")[0].length
-  // Create regex patterns based on the length
-  const regexes: RegExp[] = [
-    // Horizontal
-    new RegExp("XMAS", "g"),
-    new RegExp("SAMX", "g"),
-    // Vertical
-    new RegExp("X(?=(.|\n){" + lineLenght + "})M(?=(.|\n){" + lineLenght + "})A(?=(.|\n){" + lineLenght + "})S", "g"),
-    new RegExp("S(?=(.|\n){" + lineLenght + "})A(?=(.|\n){" + lineLenght + "})M(?=(.|\n){" + lineLenght + "})X", "g"),
-    // Diagonal
-    new RegExp("X(?=(.|\n){" + (lineLenght + 1) + "})M(?=(.|\n){" + (lineLenght + 1) + "})A(?=(.|\n){" + (lineLenght + 1) + "})S", "g"),
-    new RegExp("S(?=(.|\n){" + (lineLenght + 1) + "})A(?=(.|\n){" + (lineLenght + 1) + "})M(?=(.|\n){" + (lineLenght + 1) + "})X", "g"),
-    new RegExp("X(?=(.|\n){" + (lineLenght - 1) + "})M(?=(.|\n){" + (lineLenght - 1) + "})A(?=(.|\n){" + (lineLenght - 1) + "})S", "g"),
-    new RegExp("S(?=(.|\n){" + (lineLenght - 1) + "})A(?=(.|\n){" + (lineLenght - 1) + "})M(?=(.|\n){" + (lineLenght - 1) + "})X", "g")
-  ]
-  // Use patterns to detect occurences of XMAS
+  // Split by lines
+  const lines = data.split("\n")
+  const lineLength = lines[0].length
   let total = 0
-  regexes.forEach((regex) => {
-    const xmasOccurences = data.matchAll(regex)
-    // console.log(String(regex) + " : " + xmasOccurences.toArray().length)
-    total += xmasOccurences.toArray().length
-  })
+  for (let row = 0; row < lineLength; row++) {
+    for (let column = 0; column < lineLength; column++) {
+      // Horizontal
+      if (column <= lineLength - 4) {
+        const horizontal = lines[row].charAt(column) + lines[row].charAt(column + 1) + lines[row].charAt(column + 2) + lines[row].charAt(column + 3)
+        if (horizontal === "XMAS" || horizontal === "SAMX") {
+          total += 1
+        }
+      }
+      // Vertical & Diagonal
+      if (row <= lineLength - 4) {
+        // Vertical
+        const vertical = lines[row].charAt(column) + lines[row + 1].charAt(column) + lines[row + 2].charAt(column) + lines[row + 3].charAt(column)
+        if (vertical === "XMAS" || vertical === "SAMX") {
+          total += 1
+        }
+        // Diagonal
+        // Top-Left to Bottom-Right
+        if (column <= lineLength - 4) {
+          const topLeftBottomRight = lines[row].charAt(column) + lines[row + 1].charAt(column + 1) + lines[row + 2].charAt(column + 2) + lines[row + 3].charAt(column + 3)
+          if (topLeftBottomRight === "XMAS" || topLeftBottomRight === "SAMX") {
+            total += 1
+          }
+        }
+        // Top-Right to Bottom-Left
+        if (column >= 3) {
+          const topRightBottomLeft = lines[row].charAt(column) + lines[row + 1].charAt(column - 1) + lines[row + 2].charAt(column - 2) + lines[row + 3].charAt(column - 3)
+          if (topRightBottomLeft === "XMAS" || topRightBottomLeft === "SAMX") {
+            total += 1
+          }
+        }
+      }
+    }
+  }
+  return total
+}
+
+function part2(): number {
+  // Load data
+  const data = loadData()
+  // Split by lines
+  const lines = data.split("\n")
+  const lineLength = lines[0].length
+  let total = 0
+  // Traverse string with an offset of 1 for each side
+  for (let row = 1; row < lineLength - 1; row++) {
+    for (let column = 1; column < lineLength - 1; column++) {
+      // Find a A character
+      if (lines[row].charAt(column) === "A") {
+        // Verify if it corresponds to an X-MAS
+        const topLeftBottomRight = lines[row - 1].charAt(column - 1) + lines[row].charAt(column) + lines[row + 1].charAt(column + 1)
+        const topRightBottomLeft = lines[row - 1].charAt(column + 1) + lines[row].charAt(column) + lines[row + 1].charAt(column - 1)
+        if ((topLeftBottomRight === "MAS" || topLeftBottomRight === "SAM") && (topRightBottomLeft === "MAS" || topRightBottomLeft === "SAM")) {
+          total += 1
+        }
+      }
+    }
+  }
   return total
 }
 
 export function resolve(): void {
-  console.log("Part 1 result : " + part1() + " (expected : 2297)")
-  // console.log("Part 2 result : " + part2())
+  console.log("Part 1 result : " + part1())
+  console.log("Part 2 result : " + part2())
 }
 
 runAoc(resolve)
